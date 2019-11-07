20508e2df744c8dc1a4b7509598ffa35af25ac45
JaviEpi <javi.epi10@gmail.com>
2019-11-07 19:19:56 +0100

    cambios

---
+++ 01.ejercicios_Secuenciales/Ej17Ciclista.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Ej17Ciclista
+ */
+public class Ej17Ciclista {
+
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    s.close();
+}
+}
