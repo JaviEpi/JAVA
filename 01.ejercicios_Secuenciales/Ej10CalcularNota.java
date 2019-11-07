9aa253e7833202b202433844463d06a9c149f0b4
JaviEpi <javi.epi10@gmail.com>
2019-11-07 12:19:35 +0100

    cambios

--- 01.ejercicios_Secuenciales/Ej10CalcularNota.java
+++ 01.ejercicios_Secuenciales/Ej10CalcularNota.java
@@ -10,7 +10,31 @@ public class Ej10CalcularNota {
 
 public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
-    
+    double parcial_1;
+    double parcial_2;
+    double parcial_3;
+    double media_parciales;
+    double examen_final;
+    double trabajo_final;
+    double nota_final;
+
+    System.out.print("Nota del primer parcial: ");
+    parcial_1 = s.nextDouble();
+    System.out.print("Nota del segundo parcial: ");
+    parcial_2 = s.nextDouble();
+    System.out.print("Nota del tercer parcial: ");
+    parcial_3 = s.nextDouble();
+    media_parciales = (parcial_1+parcial_2+parcial_3)/3;
+
+    System.out.print("Nota examen final: ");
+    examen_final = s.nextDouble();
+    System.out.print("Nora del trabajo final: ");
+    trabajo_final = s.nextDouble();
+
+    nota_final = ((media_parciales*0.55)+(examen_final*0.3)+(trabajo_final*0.15));
+    System.out.println("La nota final es "+nota_final);
+
+
     s.close();
 }
 }
---
+++ 01.ejercicios_Secuenciales/Ej11Distancia.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Ej11Distancia:  Pide al usuario dos números y muestra la "distancia" entre ellos (el valor absoluto de su diferencia, de modo que el resultado sea siempre positivo).
+ */
+public class Ej11Distancia {
+public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int numero_1;
+    int numero_2;
+
+    System.out.print("Valor del primer punto: ");
+    numero_1 = s.nextInt();
+    System.out.print("Valor del segundo punto: ");
+    numero_2 = s.nextInt();
+    
+    System.out.println("La distancia entre los punto es: " +Math.abs(numero_1-numero_2));
+    
+    s.close();
+}
+
+    
+}
