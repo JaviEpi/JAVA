258d1c03c416b9adc00ec2ddbbd9b2de610c58df
JaviEpi <javi.epi10@gmail.com>
2019-11-11 23:00:09 +0100

    cambios

---
+++ 03.ejercicios_estructuras_repetitivas/Ej07Financiacion.java
@@ -0,0 +1,11 @@
+
+/**
+ * Ej07Financiacion: Una persona adquirió un producto para pagar en 20 meses. El primer mes pagó 10 €, el segundo 20 €, el tercero 40 € y así sucesivamente. Realizar un programa para determinar cuánto debe pagar mensualmente y el total de lo que pagará después de los 20 meses.
+
+ */
+public class Ej07Financiacion {
+
+public static void main(String[] args) {
+    
+}
+}
---
+++ 03.ejercicios_estructuras_repetitivas/Ej08Cronometro.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.TimeUnit;
+ 
+
+/**
+ * Ej08Cronometro: Hacer un programa que muestre un cronometro, indicando las horas, minutos y segundos.
+
+ */
+public class Ej08Cronometro {
+
+public static void main(String[] args) {
+    int hora;
+    int minuto;
+    int segundo;
+    String borraAnterior = "\b\b\b\b\b\b\b\b";  
+
+    // horas
+    for (hora=0; hora<=23; hora++) {
+      // minutos
+      for (minuto=0; minuto<=59; minuto++) {
+        // segundos
+        for (segundo=0; segundo<=59; segundo++) {
+        
+          System.out.print(borraAnterior+hora+":"+minuto+":"+segundo);
+          TimeUnit.SECONDS.sleep(1);
+        }
+
+      }
+    }
+
+    
+}
+}
