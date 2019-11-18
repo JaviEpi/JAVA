9eb5ff4c8aa220901b33da1e9b661b6739260afa
JaviEpi <javi.epi10@gmail.com>
2019-11-18 11:10:05 +0100

    cambios

--- 03.ejercicios_estructuras_repetitivas/Ej08Cronometro.java
+++ 03.ejercicios_estructuras_repetitivas/Ej08Cronometro.java
@@ -1,31 +1,29 @@
-//import java.util.concurrent.TimeUnit;
- 
 
 /**
  * Ej08Cronometro: Hacer un programa que muestre un cronometro, indicando las horas, minutos y segundos.
 
  */
+import java.util.concurrent.TimeUnit;
+
 public class Ej08Cronometro {
 
-public static void main(String[] args) {
-    int hora;
-    int minuto;
-    int segundo;
-    String borraAnterior = "\b\b\b\b\b\b\b\b";  
+public static void main(String[] args) throws InterruptedException{
+  int hora;
+  int minuto;
+  int segundo;
+  String borra_anterior = "\b\b\b\b\b\b\b\b";  
 
-    // horas
-    for (hora=0; hora<=23; hora++) {
+  // horas
+  for (hora=0; hora<=23; hora++) {
       
-      for (minuto=0; minuto<=59; minuto++) {
+    for (minuto=0; minuto<=59; minuto++) {
         
-        for (segundo=0; segundo<=59; segundo++) {
-          System.out.print(borraAnterior+hora+":"+minuto+":"+segundo);
-          //TimeUnit.SECONDS.sleep(1);
-        }
-
+      for (segundo=0; segundo<=59; segundo++) {
+          System.out.printf("%02d:%02d:%02d", hora,minuto,segundo);
+          TimeUnit.SECONDS.sleep(1);
+          System.out.print(borra_anterior);
       }
     }
-
-    
+  }
 }
 }
